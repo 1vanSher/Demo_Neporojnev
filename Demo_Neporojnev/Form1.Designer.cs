f4f5370e2403dd7baf9cb0ff7bf95328f706a0b7
Admin <Admin@DESKTOP-6UR37H6>
2024-03-16 11:46:04 +0300

    Добавьте файлы проекта.

---
+++ Demo_Neporojnev/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Demo_Neporojnev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            flowLayoutPanelProducts = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(611, 17);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 20);
+            label3.TabIndex = 16;
+            label3.Text = "Сортировка";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(336, 17);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 20);
+            label2.TabIndex = 15;
+            label2.Text = "Фильтрация";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(52, 20);
+            label1.TabIndex = 14;
+            label1.Text = "Поиск";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(611, 40);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(190, 28);
+            comboBox2.TabIndex = 13;
+            comboBox2.TextChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(336, 40);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(223, 28);
+            comboBox1.TabIndex = 12;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox1.TextChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 40);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(302, 29);
+            textBox1.TabIndex = 11;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Location = new Point(534, 616);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(267, 40);
+            flowLayoutPanel2.TabIndex = 18;
+            // 
+            // flowLayoutPanelProducts
+            // 
+            flowLayoutPanelProducts.AutoScroll = true;
+            flowLayoutPanelProducts.Location = new Point(0, 86);
+            flowLayoutPanelProducts.Name = "flowLayoutPanelProducts";
+            flowLayoutPanelProducts.Size = new Size(801, 524);
+            flowLayoutPanelProducts.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(806, 664);
+            Controls.Add(flowLayoutPanel2);
+            Controls.Add(flowLayoutPanelProducts);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanelProducts;
+    }
+}
---
+++ Demo_Neporojnev/Form2.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Demo_Neporojnev
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label9 = new Label();
+            textBox9 = new TextBox();
+            label8 = new Label();
+            textBox8 = new TextBox();
+            label7 = new Label();
+            textBox7 = new TextBox();
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(178, 121);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 39;
+            button1.Text = "Отправить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(1348, 44);
+            label9.Name = "label9";
+            label9.Size = new Size(118, 20);
+            label9.TabIndex = 38;
+            label9.Text = "mincostforagent";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(1335, 67);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(125, 27);
+            textBox9.TabIndex = 37;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(1145, 44);
+            label8.Name = "label8";
+            label8.Size = new Size(197, 20);
+            label8.TabIndex = 36;
+            label8.Text = "productionworkshopnumber";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(1145, 67);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 27);
+            textBox8.TabIndex = 35;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(964, 43);
+            label7.Name = "label7";
+            label7.Size = new Size(164, 20);
+            label7.TabIndex = 34;
+            label7.Text = "productionpersoncount";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(964, 67);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 27);
+            textBox7.TabIndex = 33;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(833, 44);
+            label6.Name = "label6";
+            label6.Size = new Size(51, 20);
+            label6.TabIndex = 32;
+            label6.Text = "image";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(833, 67);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 31;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(702, 44);
+            label5.Name = "label5";
+            label5.Size = new Size(83, 20);
+            label5.TabIndex = 30;
+            label5.Text = "description";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(702, 67);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 29;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(571, 43);
+            label4.Name = "label4";
+            label4.Size = new Size(101, 20);
+            label4.TabIndex = 28;
+            label4.Text = "articlenumber";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(571, 66);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 27;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(440, 43);
+            label3.Name = "label3";
+            label3.Size = new Size(103, 20);
+            label3.TabIndex = 26;
+            label3.Text = "producttypeid";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(440, 66);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 25;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(309, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 20);
+            label2.TabIndex = 24;
+            label2.Text = "title";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(309, 67);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 23;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(9, 66);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 22;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(178, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(22, 20);
+            label1.TabIndex = 21;
+            label1.Text = "id";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(178, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 20;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1546, 182);
+            Controls.Add(button1);
+            Controls.Add(label9);
+            Controls.Add(textBox9);
+            Controls.Add(label8);
+            Controls.Add(textBox8);
+            Controls.Add(label7);
+            Controls.Add(textBox7);
+            Controls.Add(label6);
+            Controls.Add(textBox6);
+            Controls.Add(label5);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form2";
+            Text = "Form2";
+            FormClosed += Form2_FormClosed;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label9;
+        private TextBox textBox9;
+        private Label label8;
+        private TextBox textBox8;
+        private Label label7;
+        private TextBox textBox7;
+        private Label label6;
+        private TextBox textBox6;
+        private Label label5;
+        private TextBox textBox5;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label2;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
